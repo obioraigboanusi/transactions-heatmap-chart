880e5db94f442cea629b6da0ac1ca05286195bed
Obiora Igboanusi <cwertlinks@gmail.com>
2022-10-26 10:53:14 +0100

    feat: create month components

---
+++ src/components/Month/Month.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Month() {
+  return (
+    <div>Month</div>
+  )
+}
+
+export default Month
---
+++ src/components/Months/Months.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Months() {
+  return (
+    <div>Months</div>
+  )
+}
+
+export default Months
