617cd438ef7ce505cb3c474e20b5db62c093f15e
Obiora Igboanusi <cwertlinks@gmail.com>
2022-10-27 04:42:40 +0100

    feat: add tooltip

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -1,33 +1,54 @@
 import ReactTooltip from "react-tooltip";
 import styled from "styled-components";
+import moment from "moment";
 import { useHeatMapContext } from "../HeatMap/HeatMap";
 
+const baseId = "transaction-cell-";
+
 function Cell({ index }: { index: number }) {
-  const { sortedData, maxSum, minSum } = useHeatMapContext();
-  const { sum } = sortedData[index + 1] || {};
+  const { sortedData, maxSum, minSum, year } = useHeatMapContext();
+  const { sum, items, date } = sortedData[index + 1] || {};
 
   const bgColor =
     sum > 0
-      ? `rgba(255, 0, 0, ${sum / maxSum})`
+      ? `rgba(0, 128, 0, ${sum / maxSum})`
       : sum < 0
-      ? ` rgba(0, 128, 0, ${sum / minSum})`
+      ? `rgba(255, 0, 0, ${sum / minSum})`
       : null;
+  const title = date
+    ? moment(date).format("ddd, LL")
+    : moment(new Date(year?.toString())).add(index, "days").format("ddd, LL");
 
-  return <StyledCell $bgColor={bgColor} />;
+  return (
+    <>
+      <StyledCell data-tip data-for={baseId + index} $bgColor={bgColor} />
+      <ReactTooltip id={baseId + index} place="top" type="dark" effect="float">
+        <h3>{title}</h3>
+        {date ? (
+          <>
+            <span>Sum: {sum}</span>
+            <br />
+            <span>Transactions: {items?.length}</span>
+          </>
+        ) : (
+          <span>No transactions</span>
+        )}
+      </ReactTooltip>
+    </>
+  );
 }
+
 interface Props {
   $bgColor: string | null;
 }
 
 const StyledCell = styled.li<Props>`
-  height: 10px;
-  width: 10px;
-  border: 1px solid rgba(0, 0, 0, 0.1);
+  height: 12px;
+  width: 12px;
   margin: 2px;
-  border-radius: 2px;
-  background-color: ${({ $bgColor }) => $bgColor || "rgba(0, 0, 0, 0.05)"};
+  background-color: ${({ $bgColor }) => $bgColor || "lightgray"};
   &:hover {
-    border: 1px solid rgba(0, 0, 0, 0.3);
+    background-color: rgba(0, 0, 0, 0.75);
   }
 `;
 
